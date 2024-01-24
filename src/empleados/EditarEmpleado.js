87ce0b75a171dd69cb16e6dd6cf1f05c49f3e141
lynnethP <lynnethpereira12@gmail.com>
2024-01-24 10:48:14 -0400

    Update EditarEmpleado

--- src/empleados/EditarEmpleado.js
+++ src/empleados/EditarEmpleado.js
@@ -1,58 +1,55 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 
-export default function AgregarEmpleado() {
-
-    let navegacion = useNavigate();
-
+export default function EditarEmpleado({ data }) {
     const [empleado, setEmpleado] = useState({
         nombre: "",
         departamento: "",
         sueldo: ""
     })
 
-    const {nombre, departamento, sueldo} = empleado
+    const { nombre, departamento, sueldo } = empleado
 
     const onInputChange = (e) => {
-        setEmpleado({...empleado, [e.target.name]: e.target.value})
+        setEmpleado({ ...empleado, [e.target.name]: e.target.value })
     }
 
     const onSubmit = async (e) => {
         e.preventDefault();
-        const urlBase = 'http://localhost:8080/rh-app/empleados';
-        await axios.post(urlBase, empleado);
-        navegacion("/");
+        // const urlBase = 'http://localhost:8080/rh-app/empleados';
+        // await axios.post(urlBase, empleado);
         // window.location.reload(true);
-        document.getElementById('btnClose').click();
-        
+        // console.log(this.form.action);
+        console.log("Data: " + data)
+
+        console.log(document.forms['form']);
     }
 
     return (
         <div className='container'>
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal fade" id="updateModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Insert Employee</h1>
-                            <button type="button" id='btnClose' className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Update Employee</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <form action='/agregar' onSubmit={(e)=> onSubmit(e)}>
+                            <form name='form' action={`/editar/${data}`} onSubmit={(e) => onSubmit(e)}>
                                 <div className="mb-3">
                                     <label htmlFor="nombre" className="form-label">Name</label>
-                                    <input type="text" className="form-control" id="nombre" name='nombre' value={nombre} onChange={(e)=> onInputChange(e)}/>
+                                    <input type="text" className="form-control" id="nombre" name='nombre' value={nombre} onChange={(e) => onInputChange(e)} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="departamento" className="form-label">Departament</label>
-                                    <input type="text" className="form-control" id="departamento" name='departamento' value={departamento} onChange={(e)=> onInputChange(e)}/>
+                                    <input type="text" className="form-control" id="departamento" name='departamento' value={departamento} onChange={(e) => onInputChange(e)} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="sueldo" className="form-label">Salary</label>
-                                    <input type="number" step="any" className="form-control" id="sueldo" name='sueldo' value={sueldo} onChange={(e)=> onInputChange(e)}/>
+                                    <input type="number" step="any" className="form-control" id="sueldo" name='sueldo' value={sueldo} onChange={(e) => onInputChange(e)} />
                                 </div><br />
                                 <div className='text-center'>
-                                    <button type="submit" className="btn btn-success me-3">Add</button>
+                                    <button type="submit" className="btn btn-success me-3">Update</button>
                                     <button type="button" className="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">Close</button>
                                 </div>
                             </form>
--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { NumericFormat } from 'react-number-format';
 import AgregarEmpleado from './AgregarEmpleado';
+import EditarEmpleado from './EditarEmpleado';
+import { Link } from 'react-router-dom';
 
 export default function ListadoEmpleados() {
 
@@ -13,6 +15,16 @@ export default function ListadoEmpleados() {
         cargarEmpleados();
     }, []);
 
+    const [id, setId] = useState("");
+
+    function onClickHandler(event, idEmp) { 
+        event.preventDefault();
+        console.log('handleClick 👉️', idEmp);
+        setId(idEmp);
+        console.log(id);        
+    }
+    // console.log('setId 👉️', id);
+
     const cargarEmpleados = async () => {
         const result = await axios.get(urlBase);    //Esperar respuesta del backEnd
         console.log("Result employees");
@@ -20,39 +32,54 @@ export default function ListadoEmpleados() {
         setEmpleados(result.data);
     }
 
+
     return (
         <div className='container'>
             <div className="container text-center" style={{ margin: "30px" }}>
                 <h2>Human Resources System</h2>
                 <br />
-                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" title='Add Employee' style={{ borderRadius: "100%"}}>
-                    <span><i class="fa-solid fa-user-plus"></i></span>
+                <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" title='Add Employee' style={{ borderRadius: "100%" }}>
+                    <span><i className="fa-solid fa-user-plus"></i></span>
                 </button>
             </div>
-                    <i class="fa-solid fa-user-pen"></i>
-                    <i class="fa-solid fa-user-xmark"></i>
 
-                    <AgregarEmpleado />
+
+            <AgregarEmpleado />
             <table className="table table-striped table-hover align-middle">
-                <thead className='table-dark'>
+                <thead className='table-dark text-center'>
                     <tr>
                         <th scope="col">Id</th>
                         <th scope="col">Empleado</th>
                         <th scope="col">Departamento</th>
                         <th scope="col">Sueldo</th>
+                        <th scope="col">Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         empleados.map((empleado, index) => (
                             < tr key={index} >
-                                <th scope="row">{empleado.idEmpleado}</th>
-                                <td>{empleado.nombre}</td>
-                                <td>{empleado.departamento}</td>
-                                <td>
+                                <th className='text-center' scope="row">{empleado.idEmpleado}</th>
+                                <td className='text-center'>{empleado.nombre}</td>
+                                <td className='text-center'>{empleado.departamento}</td>
+                                <td className='text-center'>
                                     <NumericFormat value={empleado.sueldo}
                                         displayType='text' thousandSeparator=','
                                         prefix='$' decimalScale={2} fixedDecimalScale />
+                                </td>
+                                <td>
+                                    <div className='text-center'>
+                                        {/* <button onClick={(event) => onClickHandler(event, empleado.idEmpleado)}>{empleado.idEmpleado}</button> */}
+                                        {/* <Link to={`editar/${empleado.idEmpleado}`} className='btn btn-warning btn-sm'>Update</Link> */}
+                                        <button onClick={(event) => onClickHandler(event, empleado.idEmpleado)} style={{ border: "none", background: "none" }} title='Update' data-bs-toggle="modal" data-bs-target="#updateModal">
+                                            <span><i className="fa-solid fa-user-pen mx-3 text-primary"></i></span>
+                                        </button>
+                                        <button onClick={(event) => onClickHandler(event, empleado.idEmpleado)} style={{ border: "none", background: "none" }} title='Delete'>
+                                            <span><i className="fa-solid fa-user-xmark text-danger"></i></span>
+                                        </button>
+                                    </div>
+                                    < EditarEmpleado data={id} />
+
                                 </td>
                             </tr>
                         ))
